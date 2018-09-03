553b439820bb18261096026fc85bce785f99c054
Senzdamsik <fakhri_kurniawan1@yahoo.com>
2018-09-03 09:19:21 +0700

    File users.js sudah betul utk kelompok nama data, selanjutnya benerin file router.js untuk kelompok nama data

--- router.js
+++ router.js
@@ -4,12 +4,9 @@ var fs = require('fs');
 var DataFrame = require('dataframe-js').DataFrame;
 var dataForge = require('data-forge');
 
-var ExportToCsv = require('export-to-csv');
-
-
 /* GET users listing. */
-router.get('/:nama_data1', function(req, res, next) {
-    var nama_data2 = req.params.nama_data1;
+router.get('/:nama_kelompok_data1', function(req, res, next) {
+    var nama_data2 = req.params.nama_kelompok_data1;
     var nama_data3 = nama_data2.split("-").join(" ")
     var nama_data4 = String("'"+nama_data3+"'")
     var query = 'select a.id, b.nama as nama_data, a.data_x as data_x, a.data_y as data_y, ' +
@@ -31,63 +28,63 @@ router.get('/:nama_data1', function(req, res, next) {
                 var tampung_sementara = []
                 for(var aa = 0; aa < results.length; aa++){   
                 
-                df = Object.values(results[aa])
-                data_x1 = df[2].split(",")
-                var tampung_x = []
-                for(var i = 0; i < data_x1.length; i++){tampung_x.push(data_x1[i])}
-                
-                data_y1 = df[3].split(",")
-                var tampung_y = []
-                for(var j = 0; j < data_y1.length; j++){tampung_y.push(data_y1[j])}
+                    df = Object.values(results[aa])
+                    data_x1 = df[2].split(",")
+                    var tampung_x = []
+                    for(var i = 0; i < data_x1.length; i++){tampung_x.push(data_x1[i])}
+                    
+                    data_y1 = df[3].split(",")
+                    var tampung_y = []
+                    for(var j = 0; j < data_y1.length; j++){tampung_y.push(data_y1[j])}
 
-                var tampung_id = []
-                for(var k = 0; k < data_x1.length; k++){tampung_id.push(df[0])}
+                    var tampung_id = []
+                    for(var k = 0; k < data_x1.length; k++){tampung_id.push(df[0])}
 
-                var tampung_nama_data = []
-                for(var l = 0; l < data_x1.length; l++){tampung_nama_data.push(df[1])}
-
-
-
-                var tampung_nama_produk = []
-                for(var m = 0; m < data_x1.length; m++){tampung_nama_produk.push(df[4])}
-
-                var tampung_item = []
-                for(var n = 0; n < data_x1.length; n++){tampung_item.push(df[5])}
-
-                var tampung_negara = []
-                for(var p = 0; p < data_x1.length; p++){tampung_negara.push(df[6])}
-
-                var tampung_provinsi = []
-                for(var q = 0; q < data_x1.length; q++){tampung_provinsi.push(df[7])}
-
-                var tampung_kota = []
-                for(var r = 0; r < data_x1.length; r++){tampung_kota.push(df[8])}
-
-                var tampung_satuan = []
-                for(var s = 0; s < data_x1.length; s++){tampung_satuan.push(df[9])}
+                    var tampung_nama_data = []
+                    for(var l = 0; l < data_x1.length; l++){tampung_nama_data.push(df[1])}
 
 
-                var tampung_sumber = []
-                var hasil_split = df[10].split(",")
-                for(var t = 0; t < data_x1.length; t++){tampung_sumber.push(hasil_split[0])}
-                
 
-                var lf = new dataForge.DataFrame({
-                    columns: {
-                        Nama_Data: tampung_nama_data,
-                        Data_X: tampung_x,
-                        Data_Y: tampung_y,
-                        Nama_Produk: tampung_nama_produk,
-                        Item: tampung_item,
-                        Negara: tampung_negara,
-                        Provinsi: tampung_provinsi,
-                        Kota: tampung_kota,
-                        Satuan: tampung_satuan,
-                        Sumber: tampung_sumber,
-                    },
-                });
+                    var tampung_nama_produk = []
+                    for(var m = 0; m < data_x1.length; m++){tampung_nama_produk.push(df[4])}
 
-                tampung_sementara.push(lf)
+                    var tampung_item = []
+                    for(var n = 0; n < data_x1.length; n++){tampung_item.push(df[5])}
+
+                    var tampung_negara = []
+                    for(var p = 0; p < data_x1.length; p++){tampung_negara.push(df[6])}
+
+                    var tampung_provinsi = []
+                    for(var q = 0; q < data_x1.length; q++){tampung_provinsi.push(df[7])}
+
+                    var tampung_kota = []
+                    for(var r = 0; r < data_x1.length; r++){tampung_kota.push(df[8])}
+
+                    var tampung_satuan = []
+                    for(var s = 0; s < data_x1.length; s++){tampung_satuan.push(df[9])}
+
+
+                    var tampung_sumber = []
+                    var hasil_split = df[10].split(",")
+                    for(var t = 0; t < data_x1.length; t++){tampung_sumber.push(hasil_split[0])}
+                    
+
+                    var lf = new dataForge.DataFrame({
+                        columns: {
+                            Nama_Data: tampung_nama_data,
+                            Data_X: tampung_x,
+                            Data_Y: tampung_y,
+                            Nama_Produk: tampung_nama_produk,
+                            Item: tampung_item,
+                            Negara: tampung_negara,
+                            Provinsi: tampung_provinsi,
+                            Kota: tampung_kota,
+                            Satuan: tampung_satuan,
+                            Sumber: tampung_sumber,
+                        },
+                    });
+
+                    tampung_sementara.push(lf)
                 }
 
                 var df0 = new dataForge.DataFrame({
